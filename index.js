b6097d8de594ab648d10f054ec520a2a440819f5
Gabriela Vilela Heimer <gvh@ug3c25.windows.cin.ufpe.br>
2019-11-07 11:46:44 -0300

    fazendo linhas entro pontos corretamente

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var c = document.getElementById("canvas");
 var cpBut = document.getElementById("cpBut");
 var ctx = c.getContext("2d");
 var clickedCp = false;
+// variaveis globais que uso no addControlPoint - nao me julgue pf
+var pointCount, prevPoint;
 
 function addControlPoint(e){
     // ajeitando o problema de pegar a posicao com flexbox
     var posX = e.pageX - c.getBoundingClientRect().left;
     var posY = e.pageY - c.getBoundingClientRect().top;
 
+    pointCount++;
     ctx.beginPath();
     ctx.arc(posX, posY, 2, 0, 2 * Math.PI, false);
     ctx.fillStyle = 'black';
@@ -15,12 +18,22 @@ function addControlPoint(e){
     ctx.stroke();
     ctx.arc(posX, posY, 6, 0, 2 * Math.PI, false);
     ctx.stroke();
+
+    // se tiver mais de um ponto, eu faco uma linha
+    if(pointCount > 1){
+        ctx.moveTo(prevPoint[0], prevPoint[1]);
+        ctx.lineTo(posX, posY);
+        ctx.stroke();
+    }
+    prevPoint = [posX, posY];
 }
 
 function cpClick(){
     if(!clickedCp){
         clickedCp = true;
+        pointCount = 0;
         cpBut.innerHTML = "Criar curva";
+
         c.addEventListener("click", addControlPoint);
     } else {
         clickedCp = false;
